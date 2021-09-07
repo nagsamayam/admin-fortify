a727d615ed899276c5a8247e38e7fa5f1ea32d8f
Steve Waugh <stevewaughbitrix@gmail.com>
2021-09-07 17:04:32 +0530

    Load class

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -5,6 +5,7 @@ namespace NagSamayam\AdminFortify\Tests;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use NagSamayam\AdminFortify\AdminFortifyServiceProvider;
 use Orchestra\Testbench\TestCase as Orchestra;
+use CreateAdminsTable;
 
 class TestCase extends Orchestra
 {
